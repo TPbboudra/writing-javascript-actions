9271bdf9703804be4bf8bf824c617041623b04c6
Benjamin Boudra <benjamin.boudra@travelport.com>
2022-05-25 15:53:21 -0600

    fix issue with imports

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -3,10 +3,9 @@ import getJoke from "./joke.js";
 // import { setOutput } from "@actions/core";
 
 async function run() {
-  console.log("running main.js")
   const joke = await getJoke();
   console.log(joke);
-//  setOutput("joke-output", joke);
+  setOutput("joke-output", joke);
 }
 
 run();
