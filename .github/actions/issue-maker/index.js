f78277e69649da134415ec8d0654a164d9150c33
Benjamin Boudra <benjamin.boudra@travelport.com>
2022-05-26 11:07:25 -0600

    Update to fix permissions issue

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,14 +7,14 @@ async function run() {
       const jokeBody = getInput("joke");
       const token = getInput("repo-token");
   
-      const octokit = getOctokit(token);
+      // const octokit = getOctokit(token);
   
-      const newIssue = await octokit.rest.issues.create({
-          repo: context.repo.repo,
-          owner: context.repo.owner,
-          title: issueTitle,
-          body: jokeBody
-      });
+      // const newIssue = await octokit.rest.issues.create({
+      //     repo: context.repo.repo,
+      //     owner: context.repo.owner,
+      //     title: issueTitle,
+      //     body: jokeBody
+      // });
     } catch (err) {
         setFailed(err.message);
     }
